f28012ab19244214311fa8c68af3fd668a1d115d
pir <hgstudy_@naver.com>
2023-01-18 00:26:54 +0900

    Initial commit

---
+++ src/main/java/pir/demo/circuitbreakermonitoring/CircuitbreakerMonitoringApplication.java
@@ -0,0 +1,13 @@
+package pir.demo.circuitbreakermonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitbreakerMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircuitbreakerMonitoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pir/demo/circuitbreakermonitoring/CircuitbreakerMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package pir.demo.circuitbreakermonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitbreakerMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
